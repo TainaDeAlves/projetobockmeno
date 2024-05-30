ec9105f1ac0fc8c6af86d96ea09398ca3d077f6d
Taina De Alves <tainadesouzalves@gmail.com>
2024-05-29 21:46:30 -0300

    seed da tabela categoria e usuarios

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('pt_BR');
+        $categorias=[
+            'Tecnologia',
+            'Mecanico',
+            'Eletricista',
+            'Pintor',
+            'Pedreiro',
+            'Jardineiro'
+        ];
+        foreach($categorias as $categoria){
+            DB::table('categorias')->insert([
+                'titulo'=>$categoria,
+                'imagem'=> asset('img/categoria-carro.jpg'),
+                'descricao' =>$faker->sentence()
+            ]);
+        }
+    }
+}
